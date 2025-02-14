043e9694d170c456b2c3e2008e25a03a8e0a1507
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-02-14 13:12:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    // Ensure that the build generates an index.html in the output
+    emptyOutDir: true
   }
 });
